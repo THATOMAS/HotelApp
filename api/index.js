05d3cf63e7bcba328f07a2dfdd1b9c82b0c408f9
THATOMAS <chachumashori@gmail.com>
2023-01-10 09:18:37 +0200

    Created a database models folder for API requests

--- api/index.js
+++ api/index.js
@@ -2,6 +2,12 @@ import express from "express"
 import dotenv from "dotenv"
 import mongoose from "mongoose"
 
+import authRoute from "./routes/auth.js"
+import hotelsRoute from "./routes/hotels.js"
+import roomsRoute from "./routes/rooms.js"
+import usersRoute from "./routes/users.js"
+
+
 mongoose.set('strictQuery', false)
 
 const app = express()
@@ -10,24 +16,31 @@ dotenv.config()
 const connect = async ()=>{ 
 try{
 	await mongoose.connect(process.env.MONGO);
-	console.log("Connected to mongoDB. ")
+	console.log("Connection to mongoDB established. ")
 }
 catch(error){
 	throw error}
 }
 
 mongoose.connection.on("disconnected" , ()=>{
-	console.log("mongoDB disconnected")
+	console.log("disconnected")
 })
 
 mongoose.connection.on("connected" , ()=>{
-	console.log("mongoDB connected! ")
+	console.log("connected! ")
 })
-
 
 app.get("/",(req,res) =>{
 	res.send("Hello first request")
 })
+
+app.use("/api/auth",authRoute)
+app.use("/api/hotels",hotelsRoute)
+app.use("/api/rooms",roomsRoute)
+app.use("/api/users",usersRoute)
+
+
+
 
 app.listen(8000,()=>{
 	connect()
